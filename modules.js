d67b86e89b09e6819ce7a790f3059cfb24a64c36
Rapha <rapha.pare@gmail.com>
2017-11-30 14:27:06 +0100

    Merge 2

--- modules.js
+++ modules.js
@@ -1,6 +1,5 @@
 var request = require('request');
 
-<<<<<<< HEAD
 
 module.exports = {
     slack: function(args) {
@@ -11,15 +10,6 @@ module.exports = {
                 if (!error && response.statusCode == 200) {
                     console.log(body)
                 }
-=======
-var slack = function(args) {
-    request.post(
-        'https://hooks.slack.com/services/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',
-        { json: { text: 'Vous avez reçu un virement' } },
-        function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                console.log(body)
->>>>>>> e6eceed3da3f88c65d329d9d8b689e3e7f4b51b7
             }
         );
     }
